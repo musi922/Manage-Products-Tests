9c19c4c837c4de674c4b9f15a523a14f46bb669e
Richard Musime <richardmusime6@gmail.com>
2025-01-08 11:18:55 +0200

    added the tests for the page title

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -4,8 +4,18 @@ import { browser,  $} from '@wdio/globals'
 describe('Main Page Tests', () => {
     it('should open the main page', async() => {
         await browser.url('http://localhost:8080/index.html')
-        await browser.pause(10000)
+        await browser.pause(5000)
     });
+    it('should check the title of the page', async () => {        
+        const title = await $('.manageProduct')
+        await title.waitForExist({ timeout: 10000 })
+        await title.waitForDisplayed({ timeout: 10000 })
+        
+        const titleText = await title.getText()
+        console.log('Found title text:', titleText)
+        expect(titleText).toBe('Manage Products')
+    });
+    
     
     
 });

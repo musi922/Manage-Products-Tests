8e35656d770154078aecafd82b359098c065f092
Richard Musime <richardmusime6@gmail.com>
2025-01-13 16:59:20 +0200

    Added the mazimize window before

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -2,7 +2,11 @@ import { expect } from '@wdio/globals'
 import { browser,  $} from '@wdio/globals'
 
 describe('Main Page Tests', () => {
+    before(async () => {
+        await browser.maximizeWindow();
+    });
     it('should open the main page', async() => {
+        
         await browser.url('http://localhost:8080/index.html')
         await browser.pause(3000)
     });
